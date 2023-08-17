95d90436c3f2be98b75b8c1c5b5fe753dae5c575
Ashiq Sabith <ashiqsabith328@gmail.com>
2023-08-17 11:44:28 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# video-chat-app
+
+A simple video chat app using ![Alt text](image.png)
---
+++ server/signalling.go
@@ -0,0 +1,7 @@
+package server
+
+import "github.com/gin-gonic/gin"
+
+func CreateRoomRequestHandler(c *gin.Context) {
+
+}
